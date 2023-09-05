ecba827d73c310c41c28bbad80ec839543d1df7b
Noah Kim <dongwoo_333@hanmail.net>
2023-09-05 14:11:33 +1000

    12. Finishing the Product List

--- 01-ecomm-plain/products/src/index.js
+++ 01-ecomm-plain/products/src/index.js
@@ -7,4 +7,5 @@ for (let i = 0; i < 5; i++) {
   products += `<div>${name}</div>`;
 }
 
-console.log(products);
+// console.log(products);
+document.querySelector('#dev-products').innerHTML = products;
